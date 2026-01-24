8122687a89386e46f78a4c63854ee67e556d3384
shaggysa <gabriel.couchenour@gmail.com>
2026-01-24 11:12:03 -0600

    pin the futures with select

--- src/tools.rs
+++ src/tools.rs
@@ -39,7 +39,7 @@ macro_rules! __select_internal {
 
     // Recursive case
     ([$($arms:tt)*], $head:expr $(, $tail:expr)*) => {{
-        let mut __fut = $head;
+        let mut __fut = pin!($head);
 
         $crate::__select_internal!(
             [
