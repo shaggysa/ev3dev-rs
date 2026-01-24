b2f6d573ec590e9de9cac06600f49300e3c18baa
shaggysa <gabriel.couchenour@gmail.com>
2026-01-24 11:33:40 -0600

    try re-adding select_internal

--- src/tools.rs
+++ src/tools.rs
@@ -23,9 +23,27 @@ pub macro join($($fut:expr),+ $(,)?) {
 /// use ev3dev_rs::tools::select;
 /// select!(drive.straight(100), attachment_motor.run_until_stalled(-45))?;
 /// ```
-pub macro select($($fut:expr),+ $(,)?) {
-    let mut __futs = ($($fut),+);
-    tokio::select! {
-        $( res = &mut __futs.${index()} => res?, )+
-    }
+pub macro select($($fut:expr),+ $(,)?) {{
+    $crate::__select_internal!([], $($fut),+)
+}}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __select_internal {
+    // Final case: no more futures, expand select! with all accumulated arms
+    ([$($arms:tt)*],) => {
+        tokio::select! { $($arms)* }
+    };
+
+    // Recursive case: grab head future, add a branch, recurse on tail
+    ([$($arms:tt)*], $head:expr $(, $tail:expr)*) => {
+        tokio::pin!(__fut = $head);
+        $crate::__select_internal!(
+            [
+                $($arms)*
+                res = &mut __fut => res?,
+            ],
+            $($tail),*
+        )
+    };
 }
